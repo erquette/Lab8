e3978443bfb1d1a48e8a76a6df7ce379ea42b2e6
Cinguinique <erquette@ualberta.ca>
2023-03-08 04:51:23 -0700

    GREEN: hasCity successful build

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -31,7 +31,7 @@ public class CustomListTest {
         list.addCity(city);
         assertTrue(list.hasCity(city));
 
-        City anotherCity = new City("Kelowna", "BC");
-        assertFalse(list.hasCity(anotherCity));
+        City secondCity = new City("Vancouver", "BC");
+        assertFalse(list.hasCity(secondCity));
     }
 }
